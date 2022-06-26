820e311f9f2354b4a3123bd197bc5ab1ebc98308
Kenyon Blue <kblue1@costar.com>
2022-06-26 08:13:40 -0600

    import json file, set and display data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Grid from "./components/grid/grid";
+import './App.css'
 
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
+    <div className="w-full test flex flex-col  items-center">
+      <h1 className="text-3xl font-bold underline App-link">
         Hello world
       </h1>
       <Grid/>
--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,29 +1,56 @@
-import { AgGridReact } from 'ag-grid-react';
+import { AgGridReact } from "ag-grid-react";
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
+import Axios from "axios";
+import data from "../../data/traffic_bytes.json";
 
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Grid = () => {
+  const [rowData, setRowData] = useState([]);
+  // const [rowData, setRowData] = useState ([
+  //     {Source: 'ford', Destination: 'focus', total_bytes: 40000 },
+  //     {Source: 'caddy', Destination: 'xts', total_bytes: 50000 },
+  //     {Source: 'lambo', Destination: 'aventy', total_bytes: 60000 },
+  // ]);
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "Source" },
+    { field: "Destination" },
+    { field: "total_bytes" },
+  ]);
 
-    const rowData = [
-        {make: 'ford', model: 'focus', price: 40000 },
-        {make: 'caddy', model: 'xts', price: 50000 },
-        {make: 'lambo', model: 'aventy', price: 60000 },
-    ];
-    const columnDefs = [{field: 'make'}, {field: 'model'}, {field: 'price'}];
+  const mapData = () => {
+    let datalist = [];
+    data.data.map((x) => {
+      datalist.push({
+        Source: x.result["All_Traffic.src"],
+        Destination: x.result["All_Traffic.dest"],
+        total_bytes: x.result.sum_bytes,
+      });
+    });
+    return datalist;
+  };
+
+  useEffect(() => {
+    setRowData(mapData());
+  }, []);
+
   return (
     <>
-    <div style={{
-					height: '500px',
-					width: '600px'
-				}}
- className="ag-theme-balham">
-    <AgGridReact rowData={rowData} columnDefs={columnDefs}/>
-    </div>
+      <div
+        style={{
+          height: "500px",
+          width: "600px",
+        }}
+        //  className="ag-theme-alpine-dark">
+        className="ag-theme-balham"
+      >
+        <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
